ebd5737637c694c95279da8be780a3925a7a3859
Sean <sean@quiet.local>
2023-01-28 20:41:42 -0800

    added colors and tweakable params. todo gui

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d, Delaunay
 from perlin_noise import PerlinNoise
@@ -10,9 +10,9 @@ def distance(p1,p2):
 
 
 if __name__ == '__main__':
-    seed = 176235471
+    seed = 185948245
     np.random.seed(seed)
-    world_size = 20
+    world_size = 38
     points = []
     idx = 0
     for i in range(0,world_size):
@@ -24,47 +24,75 @@ if __name__ == '__main__':
             idx += 1
     points = np.array(points)
     vor = Voronoi(points)
-    voronoi_plot_2d(vor)
+    voronoi_plot_2d(vor, show_vertices=False)
     tri = Delaunay(points)
 
-    noise1 = PerlinNoise(octaves=10,seed=seed)
-    noise2 = PerlinNoise(octaves=20)
-    noise3 = PerlinNoise(octaves=30)
+    noise1 = PerlinNoise(octaves=3,seed=seed)
+    noise2 = PerlinNoise(octaves=6,seed=seed)
+
 
     print(len(vor.regions))
     print(len(vor.points))
 
     land = []
     for p in vor.points:
-        land.append([False, p])
+        land.append([0.0, p])
     #for r in vor.regions:
     idx = 0
 
-    scale = 10
-    for x in range(0,len(vor.points)):
-        for y in range(0,len(vor.points)):
-            noise_val = noise1([x*scale/len(vor.points),y*scale/len(vor.points)*scale])
-            noise_val += 0.45 + noise2([x/len(vor.points),y/len(vor.points)])
-            noise_val += 0.2 + noise3([x/len(vor.points),y/len(vor.points)])
-            if noise_val > 0.65:
-                land[idx] = [True, land[idx][1]]
-            elif noise_val <= 0.65:
-                land[idx] = [False, land[idx][1]]
-        #coloring cells
-        point_index = np.argmin(np.sum((points - land[idx][1]) ** 2, axis=1))
+    scale = 6
+    pic = []
+
+    ### for x in range(0,len(vor.points)):
+     #   row = []
+      #  for y in range(0,len(vor.points)):
+       #     noise_val = 0.2 + noise1([x*scale/len(vor.points)*scale,y*scale/len(vor.points)*scale])
+       #     land[idx] = [noise_val, land[idx][1]]
+       #     row.append(noise_val)
+      #  pic.append(row)
+
+     #   if idx < len(vor.points):
+       #     idx += 1
+
+    for cell in land:
+        row = []
+        # creating noise
+        x = land[idx][1][0]
+        y = land[idx][1][1]
+        noise_val = 0.2 + noise1([x * scale / len(vor.points) * scale, y * scale / len(vor.points) * scale])
+        noise_val = noise_val + noise2([x * scale / len(vor.points) * scale, y * scale / len(vor.points) * scale])
+
+        land[idx] = [noise_val, land[idx][1]]
+        row.append(noise_val)
+        pic.append(row)
+
+        if idx < len(vor.points):
+            idx += 1
+
+    for cell in land:
+
+        # coloring cells
+        point_index = np.argmin(np.sum((points - cell[1]) ** 2, axis=1))
         ridges = np.where(vor.ridge_points == point_index)[0]
         vertex_set = set(np.array(vor.ridge_vertices)[ridges, :].ravel())
         region = [x for x in vor.regions if set(x) == vertex_set][0]
         if not -1 in region:
             polygon = vor.vertices[region]
-
-            if land[idx][0] == True:
-                plt.fill(*zip(*polygon), color='green')
+            print(cell)
+            if 0.15 < cell[0] < 0.25:
+                plt.fill(*zip(*polygon), color='c')
+            elif 0.25 < cell[0] < 0.4:
+                plt.fill(*zip(*polygon), color='y')
+            elif 0.4 < cell[0] < 0.65:
+                plt.fill(*zip(*polygon), color='g')
+            elif 0.65 < cell[0] < 0.85:
+                plt.fill(*zip(*polygon), color='gray')
+            elif cell[0] > 0.85:
+                plt.fill(*zip(*polygon), color='white')
             else:
                 plt.fill(*zip(*polygon), color='blue')
-        if idx < len(vor.regions):
-            idx +=1
     #    for idx in r:
     #        print(vor.vertices[idx])
-    plt.figure(dpi=300)
+    plt.figure(dpi=500)
+    #plt.imshow(pic, cmap='gray', alpha=1)
     plt.show()
